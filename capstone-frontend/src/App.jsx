47ecd03f55a961fabefe622e7bf58ce6f3f1b7ad
arim83 <jorammercado@pursuit.org>
2024-02-10 13:54:03 -0500

    app.jsx setup

--- capstone-frontend/src/App.jsx
+++ capstone-frontend/src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { LoginPage } from "./Pages/LoginPage.jsx";
+import { UserProvider } from "./Providers/UserProvider";
+import { LoggedInPage } from "./Pages/LoggedInPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { MemoGoogleMap } from "./Components/googleMap/GoogleMap.jsx"
+import { useState } from "react";
+import ProtectedRoute from "./Components/ProtectedRoute"
+import PublicRoute from "./Components/PublicRoute"
+import NavBar from "./Components/NavBar"
+import UnderConstruction from "./Pages/UnderConstruction"
+import FourOFour from "./Pages/FourOFour"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUserPlaces, setCurrentUserPlaces] = useState(null);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header className="App-header"></header>
+      <UserProvider>
+        <Router>
+
+          <div>
+            <NavBar currentUser={currentUser}
+              setCurrentUser={setCurrentUser} />
+          </div>
+
+          <Routes>
+
+            {/* public route login */}
+            <Route path="/"
+              element={
+                <PublicRoute
+                  element={LoginPage}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                  currentUserPlaces={currentUser}Places
+                  setCurrentUserPlaces={setCurrentUserPlaces}
+                />
+              }
+            />
+
+            {/* private route - home screen of specific user */}
+            <Route
+              path="/loggedInPage"
+              element={
+                <ProtectedRoute
+                  element={LoggedInPage}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                  currentUserPlaces={currentUser}Places
+                  setCurrentUserPlaces={setCurrentUserPlaces}
+                />
+              }
+            />
+
+            {/* private route - home screen of specific user */}
+            <Route
+              path="/maps"
+              element={
+                <ProtectedRoute
+                  element={MemoGoogleMap}
+                  currentUser={currentUser}
+                  setCurrentUser={setCurrentUser}
+                  currentUserPlaces={currentUser}Places
+                  setCurrentUserPlaces={setCurrentUserPlaces}
+                />
+              }
+            />
+
+            {/* public route - page not found */}
+            <Route
+                path="*"
+                element={
+                  <PublicRoute
+                    element={FourOFour}
+                    currentUser={currentUser}
+                    setCurrentUser={setCurrentUser}
+                  />
+                }
+              />
+
+
+          </Routes>
+        </Router>
+
+      </UserProvider>
+
+
+    </div>
+  );
 }
 
-export default App
+export default App;
