610e5bf1f635ddabb8f39ff0ade6f2a08d531438
arim83 <jorammercado@pursuit.org>
2024-02-10 14:52:25 -0500

    place comp

---
+++ capstone-frontend/src/Components/Place.jsx
@@ -0,0 +1,19 @@
+
+import React from "react"
+
+export default function Place({ place }) {
+    //console.log(place)
+    return (
+        <tr>
+            <td>{place.name}</td>
+            <td>{place.vicinity}</td>
+            <td>{place.rating}</td>
+            <td>{place.opening_hours ? `\u2705` : `\u274c`}</td>
+            <td><img
+                src={place.icon}
+                alt={`icon of ${place.name}`}
+                style={{ height: "20px" }}
+            /></td>
+        </tr>
+    )
+}
