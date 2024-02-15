0dd067826b5eadd575b02d08625787f5577f911e
arim83 <jorammercado@pursuit.org>
2024-02-15 13:52:38 -0500

    working new prototype navebar

--- capstone-frontend/src/Components/NavBar.jsx
+++ capstone-frontend/src/Components/NavBar.jsx
@@ -1,92 +1,101 @@
-import { Link } from "react-router-dom";
+
 import "./NavBar.css";
+import { useNavigate } from "react-router-dom";
 import * as React from 'react';
-import {useStyletron} from 'baseui';
-import {Button} from 'baseui/button';
-import {Layer} from 'baseui/layer';
-import {ChevronDown, Delete, Overflow, Upload} from 'baseui/icon';
+import { useStyletron } from 'baseui';
+import { Button } from 'baseui/button';
+import { Layer } from 'baseui/layer';
+import { ChevronDown, Delete, Overflow, Upload, Blank } from 'baseui/icon';
+import { StyledLink } from "baseui/link";
 import {
-  AppNavBar,
-  setItemActive,
-  NavItem,
+    AppNavBar,
+    setItemActive,
+    mapItemsActive,
+    NavItem,
 } from 'baseui/app-nav-bar';
 
-export default function NavBar() {
+export default function NavBar({ currentUser, setCurrentUser }) {
     const [css] = useStyletron();
+    const navigate = useNavigate();
 
-  const [mainItems, setMainItems] = React.useState([
-    {icon: Upload, label: 'Primary A'},
-    {icon: Upload, label: 'Primary B'},
-    {
-      icon: ChevronDown,
-      label: 'Primary C',
-      navExitIcon: Delete,
-      children: [
-        {icon: Upload, label: 'Secondary A'},
-        {icon: Upload, label: 'Secondary B'},
-        {icon: Upload, label: 'Secondary C'},
-        {icon: Upload, label: 'Secondary D'},
-      ],
-    },
-    {
-      icon: ChevronDown,
-      label: 'Primary D',
-      navExitIcon: Delete,
-      children: [
+    const [mainItems, setMainItems] = React.useState([
+        { icon: Upload, label: 'Maps' },
+        { icon: Upload, label: 'Primary B' },
         {
-          icon: ChevronDown,
-          label: 'Secondary E',
-          children: [
-            {icon: Upload, label: 'Tertiary A'},
-            {icon: Upload, label: 'Tertiary B'},
-          ],
+            icon: ChevronDown,
+            label: 'Primary C',
+            navExitIcon: Delete,
+            children: [
+                { icon: Upload, label: 'Secondary A' },
+                { icon: Upload, label: 'Secondary B' },
+                { icon: Upload, label: 'Secondary C' },
+                { icon: Upload, label: 'Secondary D' },
+            ],
         },
-        {icon: Upload, label: 'Secondary F'},
-      ],
-    },
-  ]);
-  const userItems = [
-    {icon: Overflow, label: 'Account item1'},
-    {icon: Overflow, label: 'Account item2'},
-    {icon: Overflow, label: 'Account item3'},
-    {icon: Overflow, label: 'Account item4'},
-  ];
+        {
+            icon: ChevronDown,
+            label: 'Primary D',
+            navExitIcon: Delete,
+            children: [
+                {
+                    icon: ChevronDown,
+                    label: 'Secondary E',
+                    children: [
+                        { icon: Upload, label: 'Tertiary A' },
+                        { icon: Upload, label: 'Tertiary B' },
+                    ],
+                },
+                { icon: Upload, label: 'Secondary F' },
+            ],
+        },
+    ]);
 
-  const [isNavVisible, setIsNavVisible] = React.useState(false);
+    const [userItems , setUserItems] = React.useState([
+        { icon: Blank, label: 'User' },
+        { icon: Overflow, label: 'Account item2' },
+        { icon: Overflow, label: 'Account item3' },
+        { icon: Overflow, label: 'Account item4' },
+    ]);
 
-  function handleMainItemSelect(item) {
-    setMainItems((prev) => setItemActive(prev, item));
-  }
+    const [isNavVisible, setIsNavVisible] = React.useState(true);
+
+    function handleMainItemSelect(item) {
+        setMainItems((prev) => setItemActive(prev, item));
+        if(item.label==="Maps")
+            navigate('/maps');
+    }
+
+    function handleUserItemSelect(item) {
+        setUserItems((prev) => setItemActive(prev, item));
+        setMainItems((prev) => setItemActive(prev, item));
+        navigate('/loggedInPage');
+    }
     return (
         <React.Fragment>
-      <Button onClick={() => setIsNavVisible((prev) => !prev)}>
-        {isNavVisible ? 'Hide' : 'Show'} navigation bar
-      </Button>
-      {isNavVisible && (
-        <Layer>
-          <div
-            className={css({
-              boxSizing: 'border-box',
-              width: '100vw',
-              position: 'fixed',
-              top: '0',
-              left: '0',
-            })}
-          >
-            <AppNavBar
-              title="Uber Something"
-              mainItems={mainItems}
-              userItems={userItems}
-              onMainItemSelect={handleMainItemSelect}
-              onUserItemSelect={(item) => console.log('user', item)}
-              username="Umka Marshmallow"
-              usernameSubtitle="5.0"
-              userImgUrl=""
-            />
-          </div>
-        </Layer>
-      )}
-    </React.Fragment>
+            {isNavVisible && (
+                <Layer>
+                    <div
+                        className={css({
+                            boxSizing: 'border-box',
+                            width: '100vw',
+                            position: 'fixed',
+                            top: '0',
+                            left: '0',
+                        })}
+                    >
+                        <AppNavBar
+                            title="Capstone"
+                            mainItems={mainItems}
+                            userItems={userItems}
+                            onMainItemSelect={handleMainItemSelect}
+                            onUserItemSelect={(item) => handleUserItemSelect(item)}
+                            username={currentUser ? currentUser.displayName : "User"}
+                            usernameSubtitle="Pursuit Fellow"
+                            userImgUrl={currentUser ? currentUser.photoURL : ""}
+                        />
+                    </div>
+                </Layer>
+            )}
+        </React.Fragment>
     );
 };
-
