7e7bc763c4c5e36e30a246b937e5b3214c41c251
arim83 <jorammercado@pursuit.org>
2024-02-14 22:09:01 -0500

    test new navbar

--- capstone-frontend/src/App.jsx
+++ capstone-frontend/src/App.jsx
@@ -1,3 +1,17 @@
+import { Client as Styletron } from "styletron-engine-monolithic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+import { StatefulInput } from "baseui/input";
+
+const engine = new Styletron();
+
+const Centered = styled("div", {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100%",
+});
+
 import "./App.css";
 import { LoginPage } from "./Pages/LoginPage.jsx";
 import { UserProvider } from "./Providers/UserProvider";
@@ -12,81 +26,96 @@ import NavBar from "./Components/NavBar";
 import FourOFour from "./Pages/FourOFour";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// import { Scenario as AppNavBarGetUniqueIdentifier } from './app-nav-bar-get-unique-identifier.scenario';
+// import { Scenario as AppNavBarIconOverrides } from './app-nav-bar-icon-overrides.scenario';
+// import { Scenario as AppNavBarIsMainItemActive } from './app-nav-bar-is-main-item-active.scenario';
+// import { Scenario as AppNavBarMapItemToNode } from './app-nav-bar-map-item-to-node.scenario';
+// import { Scenario as AppNavBarOverrides } from './app-nav-bar-overrides.scenario';
+// import { Scenario as AppNavBarTitleNode } from './app-nav-bar-title-node.scenario';
+// import { Scenario as AppNavBarDefault } from './app-nav-bar.scenario';
+
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
   const [currentUserPlaces, setCurrentUserPlaces] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <UserProvider>
-        <Router>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        {/* <Centered> */}
+        
+          <div className="App">
+            <header className="App-header"></header>
+            <UserProvider>
+              <Router>
 
-          <div>
-            <NavBar currentUser={currentUser}
-              setCurrentUser={setCurrentUser} />
-          </div>
+                <div>
+                  <NavBar currentUser={currentUser}
+                    setCurrentUser={setCurrentUser} />
+                </div>
 
-          <Routes>
+                <Routes>
 
-            {/* public route login */}
-            <Route path="/"
-              element={
-                <PublicRoute
-                  element={LoginPage}
-                  currentUser={currentUser}
-                  setCurrentUser={setCurrentUser}
-                  currentUserPlaces={currentUser}Places
-                  setCurrentUserPlaces={setCurrentUserPlaces}
-                />
-              }
-            />
-
-            {/* private route - home screen of specific user */}
-            <Route
-              path="/loggedInPage"
-              element={
-                <ProtectedRoute
-                  element={LoggedInPage}
-                  currentUser={currentUser}
-                  setCurrentUser={setCurrentUser}
-                  currentUserPlaces={currentUser}Places
-                  setCurrentUserPlaces={setCurrentUserPlaces}
-                />
-              }
-            />
-
-            {/* private route - home screen of specific user */}
-            <Route
-              path="/maps"
-              element={
-                <ProtectedRoute
-                  element={MemoGoogleMap}
-                  currentUser={currentUser}
-                  setCurrentUser={setCurrentUser}
-                  currentUserPlaces={currentUser}Places
-                  setCurrentUserPlaces={setCurrentUserPlaces}
-                />
-              }
-            />
-
-            {/* public route - page not found */}
-            <Route
-                path="*"
-                element={
-                  <PublicRoute
-                    element={FourOFour}
-                    currentUser={currentUser}
-                    setCurrentUser={setCurrentUser}
+                  {/* public route login */}
+                  <Route path="/"
+                    element={
+                      <PublicRoute
+                        element={LoginPage}
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                        currentUserPlaces={currentUser} Places
+                        setCurrentUserPlaces={setCurrentUserPlaces}
+                      />
+                    }
                   />
-                }
-              />
 
-          </Routes>
-        </Router>
-      </UserProvider>
+                  {/* private route - home screen of specific user */}
+                  <Route
+                    path="/loggedInPage"
+                    element={
+                      <ProtectedRoute
+                        element={LoggedInPage}
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                        currentUserPlaces={currentUser} Places
+                        setCurrentUserPlaces={setCurrentUserPlaces}
+                      />
+                    }
+                  />
 
-    </div>
+                  {/* private route - home screen of specific user */}
+                  <Route
+                    path="/maps"
+                    element={
+                      <ProtectedRoute
+                        element={MemoGoogleMap}
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                        currentUserPlaces={currentUser} Places
+                        setCurrentUserPlaces={setCurrentUserPlaces}
+                      />
+                    }
+                  />
+
+                  {/* public route - page not found */}
+                  <Route
+                    path="*"
+                    element={
+                      <PublicRoute
+                        element={FourOFour}
+                        currentUser={currentUser}
+                        setCurrentUser={setCurrentUser}
+                      />
+                    }
+                  />
+
+                </Routes>
+              </Router>
+            </UserProvider>
+
+          </div>
+        {/* </Centered> */}
+      </BaseProvider>
+    </StyletronProvider>
   );
 };
 
