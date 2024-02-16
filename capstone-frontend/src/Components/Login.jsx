154e82949c4fc2fad5f3b50b0b3d08abd51564bc
arim83 <jorammercado@pursuit.org>
2024-02-16 00:48:57 -0500

    login functionality & css updated

--- capstone-frontend/src/Components/Login.jsx
+++ capstone-frontend/src/Components/Login.jsx
@@ -1,6 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../Providers/UserProvider";
 import { useNavigate } from "react-router-dom";
+import { ButtonDock } from "baseui/button-dock";
+import { Button, KIND } from "baseui/button";
+import { styled } from 'baseui';
+import { FacebookLoginButton, GoogleLoginButton } from "react-social-login-buttons";
 import { GoogleAuthProvider, signInWithPopup, getAuth, signOut, FacebookAuthProvider } from "firebase/auth";
 import {
   signInWithGoogle,
@@ -11,6 +15,19 @@ import {
 } from "../Services/FireBase";
 import "./Login.css";
 import "animate.css";
+
+const StyledTest = styled('div', {
+  position: 'flex',
+  width: '375px',
+  height: '667px',
+  border: '1px solid #ECECEC',
+  borderRadius: '12px',
+  backgroundColor: '#ECECEC',
+  overflow: 'auto',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+});
 
 export const Login = ({ currentUser,
   setCurrentUser,
@@ -30,17 +47,17 @@ export const Login = ({ currentUser,
   const handleSignInWithFacebook = () => {
     try {
       //the signInWithPopUp() method accepts ANY provider we create. This is all our authentication logic
-      signInWithPopup(auth, facebookAuth).then((result)=>{
+      signInWithPopup(auth, facebookAuth).then((result) => {
         // This gives you a Facebook Access Token. You can use it to access the Facebook API.
         const credential = FacebookAuthProvider.credentialFromResult(result);
         const accessToken = credential.accessToken;
         // fetch facebook graph api to get user actual profile picture
         fetch(`https://graph.facebook.com/${result.user.providerData[0].uid}/picture?type=large&access_token=${accessToken}`)
-        .then((response)=>response.blob())
-        .then((blob)=>{
-          setPhotoURL(URL.createObjectURL(blob));
-        })
-      }).catch((err)=>{
+          .then((response) => response.blob())
+          .then((blob) => {
+            setPhotoURL(URL.createObjectURL(blob));
+          })
+      }).catch((err) => {
         console.log(err);
       })
     } catch (err) {
@@ -49,13 +66,33 @@ export const Login = ({ currentUser,
   }
 
   return (
-    <div className="login buttons">
-      <section>
-        <div className="login">
-          <button className="btn btn-dark btn-lg animate__animated animate__rotateIn" onClick={signInWithGoogle}>Sign in With google</button>
-          <button className="btn btn-dark btn-lg animate__animated animate__rotateIn" onClick={handleSignInWithFacebook}>Sign in With facebook</button>
-          <button className="btn btn-dark btn-lg animate__animated animate__rotateIn" onClick={() => { navigate("/signup") }}>Sign up!</button>
+    <div className="login">
+      <section className="login__wrapper">
+        <div className="animate__animated animate__fadeInUp">
+          <div className="contain__buttons">
+            <StyledTest>
+              <ButtonDock className="login-button"
+                primaryAction={[
+                  <FacebookLoginButton onClick={handleSignInWithFacebook} kind={KIND.primary} key="second" className="animate__animated animate__fadeInUp login-button">
+                  </FacebookLoginButton>,
+
+                  <GoogleLoginButton onClick={signInWithGoogle} kind={KIND.primary} key="first" className="animate__animated animate__fadeInUp login-button">
+                  </GoogleLoginButton>,
+
+                  <Button onClick={() => { navigate("/signup") }} kind={KIND.secondary} key="third"
+                    className="animate__animated animate__fadeInUp login-button">
+                    Sign Up!
+                  </Button>
+                ]}
+              />
+            </StyledTest>
+
+          </div>
         </div>
+
+
+
+
       </section>
 
     </div>
