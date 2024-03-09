93eb5c48e3eba9c443ddc54d181b256fa06d1a3c
elisaul batista <elisaulbatista@pursuit.org>
2024-03-09 14:12:58 -0500

    looking the right data to use it

--- capstone-frontend/src/Pages/News.jsx
+++ capstone-frontend/src/Pages/News.jsx
@@ -1,59 +1,60 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Table } from "baseui/table-semantic";
-
+import "animate.css";
+import "./News.scss";
 function News() {
   const [gameData, setGameData] = useState([]);
-
+  console.log(gameData, "---<DATA");
   const VITE_NEWS_API = import.meta.env.VITE_NEWS_API;
   const VITE_NEWS_HOST = import.meta.env.VITE_NEWS_HOST;
   const VITE_NEWS_URL = import.meta.env.VITE_NEWS_URL;
 
   useEffect(() => {
-    const options = {
-      method: "GET",
-      url: VITE_NEWS_URL,
-      params: {
-        teamAbv: "DAL",
-        season: "2024",
-      },
-      headers: {
-        "X-RapidAPI-Key": VITE_NEWS_API,
-        "X-RapidAPI-Host": VITE_NEWS_HOST,
-      },
-    };
-    axios(options)
-      .then((response) => {
-        setGameData(response.data.body.schedule);
-      })
-      .catch((error) => {
+    const fetchData = async () => {
+      try {
+        const options = {
+          method: "GET",
+          url: VITE_NEWS_URL,
+          params: {
+            gameDate: "20240108",
+            topPerformers: "true",
+            lineups: "true",
+          },
+          headers: {
+            "X-RapidAPI-Key": VITE_NEWS_API,
+            "X-RapidAPI-Host": VITE_NEWS_HOST,
+          },
+        };
+        const response = await axios(options);
+        if (response.data.body) {
+          const games = Object.values(response.data.body);
+          setGameData(games);
+        } else {
+          console.error("Game data not found.");
+        }
+      } catch (error) {
         console.error("Error fetching data:", error);
-      });
-  }, []);
+      }
+    };
 
-  const COLUMNS = [
-    "Date",
-    "Time",
-    
-    "Season",
-    "Home Quarter 1 Score",
-    "Country Name",
-  ];
+    fetchData();
+  }, [VITE_NEWS_API, VITE_NEWS_HOST, VITE_NEWS_URL]);
 
   return (
-    <div>
-      <h1>Latest Game News</h1>
-      <Table
-        columns={COLUMNS}
-        data={gameData.map((game) => [
-          game.gameDate,
-          game.gameTime,
-         
-          game.seasonType,
-          game.homePts,
-          game.neutralSite ? "Neutral" : game.away,
-        ])}
-      />
+    <div className="news-container">
+      <h1 className="animate__animated animate__zoomIn">Latest Game News</h1>
+
+      <div className="games-list">
+        {gameData.map((game) => (
+          <div className="game-item" key={game.gameID}>
+            <div>Date: {game.gameID.split("_")[0]}</div>
+            <div>Away Team: {game.away}</div>
+            <div>Away Points: {game.awayPts}</div>
+            <div>Home Team: {game.home}</div>
+            <div>Home Points: {game.homePts}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
