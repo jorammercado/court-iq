d739d02f8c0ce3fa1701cd6020c62faee7aca4c6
Erick Tolentino <ericktolentino@pursuit.org>
2024-03-11 21:32:16 -0400

    added player card with image

--- capstone-frontend/src/Components/PlayerCard.jsx
+++ capstone-frontend/src/Components/PlayerCard.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import * as React from "react";
+import { Card, StyledBody, StyledAction, StyledThumbnail } from "baseui/card";
+import { Button } from "baseui/button";
+import { Link } from 'react-router-dom';
 
-const PlayerCard = ({ player, stats }) => {
-
+const PlayerCard = ({ player }) => {
     return (
-        <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', display: 'inline-block', width: '200px', textAlign: 'center' }}>
-            <h4>{player.firstname} {player.lastname}</h4>
-            {/* Display player's image if available */}
-           
-        
-         
-            {/* Add more player details here */}
-
-            <p>Position: {player.position}</p>
-            {/* You can add more details based on the data structure */}
-        </div>
+      <Card
+        overrides={{ Root: { style: { width: "328px", marginBottom: "20px" } } }}
+        title={`${player.player.firstname} ${player.player.lastname}`}
+      >
+        <StyledThumbnail
+          src={player.team.logo} // Assuming 'thumbnail' is the correct key; adjust as needed
+        />
+        <StyledBody>
+          {`Team: ${player.team.name}`} {/* Adjust according to your data structure */}
+        </StyledBody>
+        <StyledAction>
+          <Link to={`/player/${player.player.id}`} style={{ textDecoration: 'none' }}>
+            <Button overrides={{ BaseButton: { style: { width: "100%" } } }}>
+              View Details
+            </Button>
+          </Link>
+        </StyledAction>
+      </Card>
     );
-};
+  };
 
 export default PlayerCard;
