7e832975a08e7ffa75e57a80dba6638b1380a684
Erick Tolentino <ericktolentino@pursuit.org>
2024-03-12 19:21:41 -0400

    added

--- capstone-frontend/src/Components/PlayerCard.jsx
+++ capstone-frontend/src/Components/PlayerCard.jsx
@@ -1,29 +1,35 @@
 import * as React from "react";
 import { Card, StyledBody, StyledAction, StyledThumbnail } from "baseui/card";
 import { Button } from "baseui/button";
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const PlayerCard = ({ player }) => {
+    const navigate = useNavigate(); // Initialize useNavigate
+
+    // Function to handle button click
+    const handleButtonClick = () => {
+        navigate(`/player/${player.player.id}`, { state: { ...player.player } });
+    };
+
     return (
-      <Card
-        overrides={{ Root: { style: { width: "328px", marginBottom: "20px", backgroundColor: "#EA6607" } } }}
-        title={`${player.player.firstname} ${player.player.lastname}`}
-      >
-        <StyledThumbnail
-          src={player.team.logo} // Assuming 'thumbnail' is the correct key; adjust as needed
-        />
-        <StyledBody>
-          {`Team: ${player.team.name}`} {/* Adjust according to your data structure */}
-        </StyledBody>
-        <StyledAction>
-        <Link to={`/player/${player.player.id}`} state={player.player} >{player.player.firstname} {player.player.lastname}
-            <Button overrides={{ BaseButton: { style: { width: "100%" } } }}>
-              View Player Details
-            </Button>
-          </Link>
-        </StyledAction>
-      </Card>
+        <Card
+            overrides={{ Root: { style: { width: "328px", marginBottom: "20px", backgroundColor: "#EA6607" } } }}
+            title={`${player.player.firstname} ${player.player.lastname}`}
+        >
+            <StyledThumbnail
+                src={player.team.logo} // Adjust as needed
+            />
+            <StyledBody>
+                {`Team: ${player.team.name}`} {/* Adjust according to your data structure */}
+            </StyledBody>
+            <StyledAction>
+                {/* Updated Button to use onClick for navigation */}
+                <Button overrides={{ BaseButton: { style: { width: "100%" } } }} onClick={handleButtonClick}>
+                    View Player Details
+                </Button>
+            </StyledAction>
+        </Card>
     );
-  };
+};
 
 export default PlayerCard;
