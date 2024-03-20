5345bff082d3e3a144d4c0d9f274d50b3aed3a82
elisaul batista <elisaulbatista@pursuit.org>
2024-03-19 21:51:37 -0400

    added better style to comparate players

--- capstone-frontend/src/constants/images.js
+++ capstone-frontend/src/constants/images.js
@@ -1,8 +1,11 @@
-
+import nbaImgCourt from '../assets/backgroound.png'
 import comparateImg from '../assets/compartePageImg.png'
-
-
+import ball from '../assets/ball.png'
+import ring from '../assets/canato.png'
 
 export default {
-    comparateImg 
+    comparateImg ,
+    nbaImgCourt,
+    ball,
+    ring
 }
