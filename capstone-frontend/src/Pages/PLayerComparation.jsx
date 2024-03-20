e4d04841278ae7e933d8a1dd7720e54bc9689e4f
elisaul batista <elisaulbatista@pursuit.org>
2024-03-19 21:25:40 -0400

    added

--- capstone-frontend/src/Pages/PLayerComparation.jsx
+++ capstone-frontend/src/Pages/PLayerComparation.jsx
@@ -1,115 +1,133 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./PLayerComparation.scss";
-import images from "../constants/images";
 
 function PlayerComparison() {
-    const [player1, setPlayer1] = useState("");
-    const [player2, setPlayer2] = useState("");
-    const [player1Data, setPlayer1Data] = useState(null);
-    const [player2Data, setPlayer2Data] = useState(null);
-  
-    const PLAYER_URL = import.meta.env.VITE_PLAYER_URL;
-    const PLAYER_HOST = import.meta.env.VITE_PLAYER_HOST;
-    const PLAYER_API_KEY = import.meta.env.VITE_PLAYER_API_KEY;
-  
-    const handleSearch = async () => {
-      try {
-        const options1 = {
-          method: "GET",
-          url: PLAYER_URL,
-          params: {
-            playerName: player1,
-            statsToGet: "averages",
-          },
-          headers: {
-            "X-RapidAPI-Key": PLAYER_API_KEY,
-            "X-RapidAPI-Host": PLAYER_HOST,
-          },
-        };
-  
-        const options2 = {
-          method: "GET",
-          url: PLAYER_URL,
-          params: {
-            playerName: player2,
-            statsToGet: "averages",
-          },
-          headers: {
-            "X-RapidAPI-Key": PLAYER_API_KEY,
-            "X-RapidAPI-Host": PLAYER_HOST,
-          },
-        };
-  
-        const [response1, response2] = await Promise.all([
-          axios.request(options1),
-          axios.request(options2),
-        ]);
-  
-        setPlayer1Data(response1.data.body[0]);
-        setPlayer2Data(response2.data.body[0]);
-      } catch (error) {
-        console.error("Error searching for players:", error);
-      }
-    };
-  
-    return (
-      <div className="player-comparison-container">
-        <div className="player-card">
-          <input
-            className="player-input input1"
-            type="text"
-            placeholder="Enter Player 1 Name"
-            value={player1}
-            onChange={(e) => setPlayer1(e.target.value)}
-          />
-          {player1Data && (
-            <div className="player-info">
-              <img className="player-img" src={player1Data.espnHeadshot} alt="" />
-              <div className="info-card">
-                <h3 className="player-name">{player1Data.nbaComName}</h3>
-                <p className="player-stat"> Weight: {player1Data.weight}</p>
-                <p className="player-stat"> Height: {player1Data.height}</p>
-                <p className="player-stat"> Team: {player1Data.team}</p>
-                <p className="player-stat"> Rebounds: {player1Data.stats.reb}</p>
-                <p className="player-stat"> Points: {player1Data.stats.pts}</p>
-                <p className="player-stat"> Blocks: {player1Data.stats.blk}</p>
-              </div>
-            </div>
-          )}
-        </div>
-          
-        <div className="player-card">
-          <input
-            className="player-input input2"
-            type="text"
-            placeholder="Enter Player 2 Name"
-            value={player2}
-            onChange={(e) => setPlayer2(e.target.value)}
-          />
-          {player2Data && (
-            <div className="player-info">
-              <img className="player-img" src={player2Data.espnHeadshot} alt="" />
-              <div className="info-card">
-                <h3 className="player-name">{player2Data.nbaComName}</h3>
-                <p className="player-stat"> Weight: {player2Data.weight}</p>
-                <p className="player-stat"> Height: {player2Data.height}</p>
-                <p className="player-stat"> Team: {player2Data.team}</p>
-                <p className="player-stat"> Rebounds: {player2Data.stats.reb}</p>
-                <p className="player-stat"> Points: {player2Data.stats.pts}</p>
-                <p className="player-stat"> Blocks: {player2Data.stats.blk}</p>
-              </div>
-            </div>
-          )}
-        </div>
-        <div className="compare-button-container">
-          <button className="compare-button" onClick={handleSearch}>
-            Compare Players
-          </button>
+  const [player1, setPlayer1] = useState("");
+  const [player2, setPlayer2] = useState("");
+  const [player1Data, setPlayer1Data] = useState(null);
+  const [player2Data, setPlayer2Data] = useState(null);
+
+  const PLAYER_URL = import.meta.env.VITE_PLAYER_URL;
+  const PLAYER_HOST = import.meta.env.VITE_PLAYER_HOST;
+  const PLAYER_API_KEY = import.meta.env.VITE_PLAYER_API_KEY;
+
+  const handleSearch = async () => {
+    try {
+      const options1 = {
+        method: "GET",
+        url: PLAYER_URL,
+        params: {
+          playerName: player1,
+          statsToGet: "averages",
+        },
+        headers: {
+          "X-RapidAPI-Key": PLAYER_API_KEY,
+          "X-RapidAPI-Host": PLAYER_HOST,
+        },
+      };
+
+      const options2 = {
+        method: "GET",
+        url: PLAYER_URL,
+        params: {
+          playerName: player2,
+          statsToGet: "averages",
+        },
+        headers: {
+          "X-RapidAPI-Key": PLAYER_API_KEY,
+          "X-RapidAPI-Host": PLAYER_HOST,
+        },
+      };
+
+      const [response1, response2] = await Promise.all([
+        axios.request(options1),
+        axios.request(options2),
+      ]);
+
+      setPlayer1Data(response1.data.body[0]);
+      setPlayer2Data(response2.data.body[0]);
+    } catch (error) {
+      console.error("Error searching for players:", error);
+    }
+  };
+
+  return (
+    <div className="player-comparison-container">
+      <div className="comparison-heading-container">
+        <h1 className="comparison-heading">Compare NBA Players</h1>
+      </div>
+      <div className="comparison-instruction-container">
+        <div className="instruction-card">
+          <p className="comparison-instruction">
+            Welcome to our NBA player comparison tool! Enter the names of two
+            NBA players below and click "Compare Players" to see their detailed
+            statistics side by side.
+          </p>
         </div>
       </div>
-    );
-  }
-  
-  export default PlayerComparison;
-  
+
+      <div className="player-inputs-container">
+        <input
+          className="player-input"
+          type="text"
+          placeholder="Enter Player 1 Name"
+          value={player1}
+          onChange={(e) => setPlayer1(e.target.value)}
+        />
+        <input
+          className="player-input"
+          type="text"
+          placeholder="Enter Player 2 Name"
+          value={player2}
+          onChange={(e) => setPlayer2(e.target.value)}
+        />
+      </div>
+
+      <div className="player-cards-container">
+  <div className="player-card">
+    {player1Data && (
+      <div className="player-info">
+        <img className="player-img" src={player1Data.espnHeadshot} alt="" />
+        <div className="info-card">
+          <h3 className="player-name">{player1Data.nbaComName}</h3>
+          <p className="player-stat">Weight: {player1Data.weight}</p>
+          <p className="player-stat">Height: {player1Data.height}</p>
+          <p className="player-stat">Team: {player1Data.team}</p>
+          <p className="player-stat">Rebounds: {player1Data.stats.reb}</p>
+          <p className="player-stat">Points: {player1Data.stats.pts}</p>
+          <p className="player-stat">Blocks: {player1Data.stats.blk}</p>
+        </div>
+      </div>
+    )}
+  </div>
+
+  <div className="player-card">
+    {player2Data && (
+      <div className="player-info">
+        <img className="player-img" src={player2Data.espnHeadshot} alt="" />
+        <div className="info-card">
+          <h3 className="player-name">{player2Data.nbaComName}</h3>
+          <p className="player-stat">Weight: {player2Data.weight}</p>
+          <p className="player-stat">Height: {player2Data.height}</p>
+          <p className="player-stat">Team: {player2Data.team}</p>
+          <p className="player-stat">Rebounds: {player2Data.stats.reb}</p>
+          <p className="player-stat">Points: {player2Data.stats.pts}</p>
+          <p className="player-stat">Blocks: {player2Data.stats.blk}</p>
+        </div>
+      </div>
+    )}
+  </div>
+</div>
+
+
+      <div className="compare-button-container">
+        <button className="compare-button" onClick={handleSearch}>
+          Compare Players
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PlayerComparison;
