8e556d7a76a17f39bc67588e7e882b2449e5902a
arim83 <jorammercado@pursuit.org>
2024-03-25 06:51:52 -0400

    refactor MyWork component, testing proper netlify image locator

--- capstone-frontend/src/Components/SectionTwo.jsx
+++ capstone-frontend/src/Components/SectionTwo.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import searchImage from "../Pages/css/images/search.png"
 
 const SectionTwo = () => (
-    <section id="two" className="main style2 left dark fullscreen">
-        <div className="content box style2">
-        <header>
-                <h2>Ask Anything About Basketball Stats</h2>
-            </header>
-            <p>Our AI Delivers Instant Insights</p>
-          <div className="image-content-box">
-            <img src="src/Pages/css/images/search.png" alt="Descriptive Alt Text" />
-          </div>
-        </div>
-        <a href="#work" className="button style2 down anchored">Next</a>
-    </section>
+  <section id="two" className="main style2 left dark fullscreen">
+    <div className="content box style2">
+      <header>
+        <h2>Ask Anything About Basketball Stats</h2>
+      </header>
+      <p>Our AI Delivers Instant Insights</p>
+      <div className="image-content-box">
+        <img src={searchImage} alt="Descriptive Alt Text" />
+      </div>
+    </div>
+    <a href="#work" className="button style2 down anchored">Next</a>
+  </section>
 );
 
 export default SectionTwo;
