be89a8b9b43b1c19b67a3c0b1003b8ecee8d7759
arim83 <jorammercado@pursuit.org>
2024-04-15 19:41:53 -0400

    fix syntax error

---
+++ capstone-frontend/src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import { Block } from 'baseui/block';
+
+const Footer = () => {
+    return (
+      <div style={{
+        width: '100%',
+        padding: '20px 0',
+        textAlign: 'center',
+        backgroundColor: '#333',
+        color: '#fff',
+        borderTop: '1px solid #444',
+        marginTop: 'auto',
+      }}>
+        © {new Date().getFullYear()} My Company
+      </div>
+    );
+  };
+  
+  export default Footer;
